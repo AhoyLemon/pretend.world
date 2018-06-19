882b3297a2a5b4efcde09f51077357bde0044b75
SanguinaryNovel <SanguinaryNovel@users.noreply.github.com>
2018-06-19 16:33:55 -0400

    Cheese Fetish Strings
    Writing borderline cheese pornography strings at work, like you do.

--- js/partials/_cheesestatus.js
+++ js/partials/_cheesestatus.js
@@ -7,21 +7,29 @@ const justSawCheeseStrings = [
 const cheeseStatus = {
   close: [
     "You’re so close now, the table almost within arms reach. You can see each mustard seed gracing the surface, and the glistening cheese throwing you “come hither” looks over the guests' shoulder. It won’t be long now, my dairy love.",
-    "Your presence near the cheese log is now what could fairly be described as menacing. You run your tongue over your teeth."
+    "Your presence near the cheese log is now what could fairly be described as menacing. You run your tongue over your teeth.",
+    "The cheese is just barely beyond your reach, its golden interiors gracefully spread open to your waiting touch. If only you could hurry through this conversation.",
+    "You wipe the corner of your mouth with a furtive hand while the impersonator before you continues to prattle on about accurate props. The earthy, sharp scent of cheese fills your nostrils it's so close."
   ],
   medium: [
-    "Would the courts accept love of cheese as an acceptable excuse for manslaughter? Surely no jury could hold him accountable, as the cheese is what separates us from animals, as the animals at this party are keeping you from obtaining cheese enlightenment.",
+    "Would the courts accept love of cheese as an acceptable excuse for manslaughter? Surely no jury could hold you accountable, as the cheese is what separates us from animals, as the animals at this party are keeping you from obtaining cheese enlightenment.",
     "You reach into your pocket, thumbing the little yellow diary you keep of past cheese love affairs. Long has the pistachio fig brie peacock sculpture from last year’s Honey Soiree been your top prize, but the one before you is sure to take that spot in your heart.",
     "You relaxed your focus a bit, and notice some salami on the west end of the table. A water cracker, the salami, maybe a sliced green olive, and that wonderful, wonderful cheese. Soon this dream will become a reality."
   ],
   far: [
-    "You find yourself in physical discomfort as you reckon how far you are from the cheese log. You must close the gap."
+    "You find yourself in physical discomfort as you reckon how far you are from the cheese log. You must close the gap.",
+    "As many a great explorer from the past, you will travel any distance, face any danger, all to get close to that golden cheese.",
+    "The world drops away into a fog, the guests voices soften to dull murmurs; over it all, the gruyere on the furthest table gazes lustfully at you.",
+    "Get to the cheese table? You'd sooner arrive at the moon before finally resting your questing fingers upon those soft silvers of swiss.",
+    "Had Shakespeare written about cheese? Perhaps a sonnet sonnet or a poem of parmesean would bring this crowd to bear, and allow you to close the gap."
   ],
   any: [
     "These parties always have impressive spreads that spared no expense; figs from the middle east, the fresh caviar from Russia, and freshly baked pastries from France. All garbage for peasants. This cheese log is the true treasure.",
     "The cheese had been delicately rolled in slivered almonds, concealing the golden spread within. You sniff the air. Did those geniuses even include a hint of scallion? You must get closer.",
     "You glance past the latest impersonator back to the cheese table and gasp, as bits of the beautiful, once whole log have been damaged by rabid impersonator wolves. But none shall keep you from sampling that luscious log.",
     "A knife slices into the cheese log, and you hiss like a vampire. You watch a guest draw the cheese and cracker into his mouth, and he doesn’t even appreciate it. Not like <i>you</i> would.",
+    "Over there she lay, the most beautiful thing in the world. Nothing can compare to her beauty, her grace, her easily spreadable interior. The most expensive cheeses rolled into a log shape rested on a silver platter, soley for you.",
+    "These moments are the kind "
   ]
 
 };
--- js/partials/_partymoods.js
+++ js/partials/_partymoods.js
@@ -1,20 +1,25 @@
 const partyMoods = {
   veryGood: [
-    "Everything is very good. PLACEHOLDER TEXT DELETE THIS ONE."
+    "The guest before you is so delighted with your sharp wit and flattery they manage to stop talking about how they could have easily replaced the actor from the movie, had they only been there.",
+    "The impersonator leans forward and glances around. They press a business card into your hand and whisper about an agent with the all of the best gigs good buddy.",
+    "The impersonators around you "
   ],
   prettyGood: [
     "You give finger guns to three separate people whom you have never met. Only one of those finger gun’d makes a face like you just farted. You consider this a success."
   ],
   neutral: [
-    "NEUTRAL. PLACEHOLDER TEXT. DELETE THIS ONE."
+    "Wait staff weave through oversized props and bulky skirts while carrying cocktails painfully based off pun potential and not taste. The guests, however, are absolutely delighted.",
+    "You wonder if there had been a squabble over the party theme, every impersonator vying to have THEIR movie recreated in the space. The result was a mixture of an 80s high school prom, derelict building props on the walls, medieval glassware, and just too much glitter."
   ],
   prettyBad: [
     "The opening riff of Dee-Lite’s “Groove Is In The Heart” starts to play, and you make an utterly nonsensical joke about Josh Grobin In The Heart? Like, Josh Grobin is in your heart? Something like that. It is a <i>terrible</i> joke and everybody heard it.",
     "Nervous, you try to shift the conversation to the latest gossip, something about the Renaissance hotel using a blue cheese and mozzarella mix. But this only raises more suspicion about your intent.",
-    
+
   ],
   veryBad: [
-    "A joke about Harvey Weinstein goes <i>way</i> off the rails and comes off as overtly anti-semitic. A Scarlett Johansson impersonator asks who let you in here, and you feel sweat on your forehead."
+    "A joke about Harvey Weinstein goes <i>way</i> off the rails and comes off as overtly anti-semitic. A Scarlett Johansson impersonator asks who let you in here, and you feel sweat on your forehead.",
+    "You make an offhand comment about an Edward Scissorhands' props, asking him if he wrapped plastic safety scissors from the Dollar Store in tinfoil. He bursts into tears and runs out of the room, bits of foil falling on the ground in his wake. Everyone at the party thinks you are SUCH an asshole for mentioning this.",
+    "Unable to keep up with the flood of pointless references, you accidentally let drop that you don't <i>actually like movies.</i> The whole party goes silent, a glass falls to the floor and shatters. The quiet is only broken when someone yells Rosebud!. The crowd laughs, but does not forget."
   ],
 
   noChange: [
