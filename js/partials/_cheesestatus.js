3ad3e3559e476d9fd8ded4f6e483038b66eb1c44
Lemon <AhoyLemon@users.noreply.github.com>
2018-06-18 21:18:34 -0500

    Copied strings from doc.

--- js/partials/_cheesestatus.js
+++ js/partials/_cheesestatus.js
@@ -4,14 +4,24 @@ const justSawCheeseStrings = [
 
 
 
-const cheeseStatus = [
-  // ---- Furthest From Cheese
-  [
-
+const cheeseStatus = {
+  close: [
+    "You’re so close now, the table almost within arms reach. You can see each mustard seed gracing the surface, and the glistening cheese throwing you “come hither” looks over the guests' shoulder. It won’t be long now, my dairy love.",
+    "Your presence near the cheese log is now what could fairly be described as menacing. You run your tongue over your teeth."
   ],
-
-  // ---- Closest to cheese
-  [
-
+  medium: [
+    "Would the courts accept love of cheese as an acceptable excuse for manslaughter? Surely no jury could hold him accountable, as the cheese is what separates us from animals, as the animals at this party are keeping you from obtaining cheese enlightenment.",
+    "You reach into your pocket, thumbing the little yellow diary you keep of past cheese love affairs. Long has the pistachio fig brie peacock sculpture from last year’s Honey Soiree been your top prize, but the one before you is sure to take that spot in your heart.",
+    "You relaxed your focus a bit, and notice some salami on the west end of the table. A water cracker, the salami, maybe a sliced green olive, and that wonderful, wonderful cheese. Soon this dream will become a reality."
+  ],
+  far: [
+    "You find yourself in physical discomfort as you reckon how far you are from the cheese log. You must close the gap."
+  ],
+  any: [
+    "These parties always have impressive spreads that spared no expense; figs from the middle east, the fresh caviar from Russia, and freshly baked pastries from France. All garbage for peasants. This cheese log is the true treasure.",
+    "The cheese had been delicately rolled in slivered almonds, concealing the golden spread within. You sniff the air. Did those geniuses even include a hint of scallion? You must get closer.",
+    "You glance past the latest impersonator back to the cheese table and gasp, as bits of the beautiful, once whole log have been damaged by rabid impersonator wolves. But none shall keep you from sampling that luscious log.",
+    "A knife slices into the cheese log, and you hiss like a vampire. You watch a guest draw the cheese and cracker into his mouth, and he doesn’t even appreciate it. Not like <i>you</i> would.",
   ]
-]
+
+};
--- js/partials/_partymoods.js
+++ js/partials/_partymoods.js
@@ -1,17 +1,23 @@
 const partyMood = {
   veryGood: [
-
+    
   ],
   prettyGood: [
-
+    "You give finger guns to three separate people whom you have never met. Only one of those finger gun’d makes a face like you just farted. You consider this a success."
   ],
   neutral: [
 
   ],
   prettyBad: [
-
+    "The opening riff of Dee-Lite’s “Groove Is In The Heart” starts to play, and you make an utterly nonsensical joke about Josh Grobin In The Heart? Like, Josh Grobin is in your heart? Something like that. It is a <i>terrible</i> joke and everybody heard it.",
+    "Nervous, you try to shift the conversation to the latest gossip, something about the Renaissance hotel using a blue cheese and mozzarella mix. But this only raises more suspicion about your intent.",
+    
   ],
   veryBad: [
-    
+    "A joke about Harvey Weinstein goes <i>way</i> off the rails and comes off as overtly anti-semitic. A Scarlett Johansson impersonator asks who let you in here, and you feel sweat on your forehead."
+  ],
+
+  noChange: [
+    "In a poorly lit corner, you see an Ashton Kutcher impersonator making out with a Demi Moore impersonator. A few feet away, a Bruce Willis impersonator is seemingly unphased by all of this, which seems oddly progressive.",
   ]
-}
+};
