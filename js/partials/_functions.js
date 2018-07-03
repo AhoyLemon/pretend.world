5f231bd5279e6f980326844f449c385c96a5c2c9
Lemon <AhoyLemon@users.noreply.github.com>
2018-07-02 22:49:32 -0500

    send Google Event.

--- js/partials/_functions.js
+++ js/partials/_functions.js
@@ -110,13 +110,13 @@ function testChance(chance) {
 
 function sendEvent(c, a, l, v) {
   if (v) {
-    //ga('send', 'event', { eventCategory: c, eventAction: a, eventLabel: l, eventValue:v });
+    ga('send', 'event', { eventCategory: c, eventAction: a, eventLabel: l, eventValue:v });
     console.log('CATEGORY: '+c+', ACTION:'+a+', LABEL:'+l+', VALUE:'+v);
   } else if (l) {
-    //ga('send', 'event', { eventCategory: c, eventAction: a, eventLabel: l });
+    ga('send', 'event', { eventCategory: c, eventAction: a, eventLabel: l });
     console.log('CATEGORY: '+c+', ACTION:'+a+', LABEL:'+l);
   } else {
-    //ga('send', 'event', { eventCategory: c, eventAction: a });
+    ga('send', 'event', { eventCategory: c, eventAction: a });
     console.log('CATEGORY: '+c+', ACTION:'+a);
   }
 }
